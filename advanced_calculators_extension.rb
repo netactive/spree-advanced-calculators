e7390af1c80b03172e52612846be2427564109ab
Georg Fruhwald <fifigyuri@gmail.com>
2010-05-15 11:07:29 +0200

    Update for Spree 0.10.2

--- advanced_calculators_extension.rb
+++ advanced_calculators_extension.rb
@@ -10,17 +10,6 @@ class AdvancedCalculatorsExtension < Spree::Extension
     # app/controllers/admin/yourextension_controller
     # and that you mapped your admin in config/routes
 
-    Admin::ConfigurationsController.class_eval do
-      before_filter :add_bucket_rates_links, :only => :index
-      def add_bucket_rates_links
-        @extension_links << {
-          :link => admin_bucket_rates_path,
-          :link_text => t('bucket_rates'),
-          :description => t('bucket_rates_description')
-        }
-      end
-    end
-
     [
       Calculator::PriceBucket, 
       Calculator::WeightBucket, 
